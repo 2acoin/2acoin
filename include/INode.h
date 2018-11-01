daacf887e86edc09a455f3698af0d8f0206ab414
2ACoin Miner <miner@2acoin.org>
2018-11-01 17:14:05 -0500

    Bump build version

--- include/INode.h
+++ include/INode.h
@@ -88,6 +88,8 @@ public:
   virtual uint32_t getLocalBlockCount() const = 0;
   virtual uint32_t getKnownBlockCount() const = 0;
   virtual uint64_t getLastLocalBlockTimestamp() const = 0;
+  virtual uint64_t getNodeHeight() const = 0;
+
   virtual std::string getInfo() = 0;
   virtual void getFeeInfo() = 0;
 
