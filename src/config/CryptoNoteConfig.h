9dc7bd874d5e72051f5788897fb6c66714a92f48
2ACoin Miner <miner@2acoin.org>
2018-11-06 13:04:04 -0600

    Set next supported Fork level

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -141,7 +141,7 @@ const uint64_t FORK_HEIGHTS[] =
 };
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
-const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 2;
+const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 3;
 
 const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
 
