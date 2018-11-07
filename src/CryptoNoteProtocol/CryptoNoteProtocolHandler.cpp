52991269c58276909bd1ea82ea1e20f74feac465
2ACoin Miner <miner@2acoin.org>
2018-11-07 12:33:49 -0600

    Cleanup formatting   * Bump build version

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The 2ACoin Developers
-// 
+//
 // Please see the included LICENSE file for more information.
 
 #include "CryptoNoteProtocolHandler.h"
