339eeed6122c795b3eb866494be81d4f083f89ea
2ACoin Miner <miner@2acoin.org>
2018-12-01 13:31:17 -0600

    Reduce MIN Mixin

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -62,7 +62,7 @@ const uint64_t MAXIMUM_MIXIN_V1                              = 100;
 const uint64_t MINIMUM_MIXIN_V2                              = 0;
 const uint64_t MAXIMUM_MIXIN_V2                              = 7;
 
-const uint64_t MINIMUM_MIXIN_V3                              = 3;
+const uint64_t MINIMUM_MIXIN_V3                              = 0;
 const uint64_t MAXIMUM_MIXIN_V3                              = 3;
 
 /* The heights to activate the mixin limits at */
