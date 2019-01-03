960e7ae922f83f4f0186a4b295dfd4c36f576169
2ACoin Miner <miner@2acoin.org>
2019-01-03 16:15:27 -0600

    Switch zedwallet values from size_t to uint64_t

--- src/zedwallet/Menu.cpp
+++ src/zedwallet/Menu.cpp
@@ -43,12 +43,12 @@ std::string parseCommand(const std::vector<T> &printableCommands,
 
         try
         {
-            size_t selectionNum = std::stoi(selection);
+            int selectionNum = std::stoi(selection);
 
             /* Input is in 1 based indexing, we need 0 based indexing */
             selectionNum--;
 
-            size_t numCommands = availableCommands.size();
+            int numCommands = static_cast<int>(availableCommands.size());
 
             /* Must be in the bounds of the vector */
             if (selectionNum < 0 || selectionNum >= numCommands)
@@ -109,7 +109,7 @@ std::tuple<bool, std::shared_ptr<WalletInfo>>
         /* User wants to exit */
         if (launchCommand == "exit")
         {
-            return std::make_tuple(true, nullptr);
+            return {true, nullptr};
         }
 
         /* Handle the user input */
@@ -129,7 +129,7 @@ std::tuple<bool, std::shared_ptr<WalletInfo>>
         /* Node is down, user wants to exit */
         if (!checkNodeStatus(node))
         {
-            return std::make_tuple(true, nullptr);
+            return {true, nullptr};
         }
     
         /* If we're creating a wallet, don't print the lengthy sync process */
@@ -167,7 +167,7 @@ std::tuple<bool, std::shared_ptr<WalletInfo>>
         }
 
         /* Return the wallet info */
-        return std::make_tuple(false, walletInfo);
+        return {false, walletInfo};
     }
 }
 
@@ -280,9 +280,9 @@ void mainLoop(std::shared_ptr<WalletInfo> walletInfo, CryptoNote::INode &node)
 }
 
 template<typename T>
-void printCommands(const std::vector<T> &commands, int offset)
+void printCommands(const std::vector<T> &commands, size_t offset)
 {
-    int i = 1 + offset;
+    size_t i = 1 + offset;
 
     std::cout << std::endl;
 
@@ -317,7 +317,7 @@ std::string parseCommand(const std::vector<AdvancedCommand> &printableCommands,
                          std::shared_ptr<WalletInfo> walletInfo);
 
 template
-void printCommands(const std::vector<Command> &commands, int offset);
+void printCommands(const std::vector<Command> &commands, size_t offset);
 
 template
-void printCommands(const std::vector<AdvancedCommand> &commands, int offset);
+void printCommands(const std::vector<AdvancedCommand> &commands, size_t offset);
