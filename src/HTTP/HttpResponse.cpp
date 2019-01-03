26f723af260125b4032847bf36363c0426f1cdfe
2ACoin Miner <miner@2acoin.org>
2019-01-03 14:41:33 -0600

    http response cors header option is now working.

--- src/HTTP/HttpResponse.cpp
+++ src/HTTP/HttpResponse.cpp
@@ -56,7 +56,6 @@ namespace CryptoNote {
 HttpResponse::HttpResponse() {
   status = STATUS_200;
   headers["Server"] = "CryptoNote-based HTTP server";
-  headers["Access-Control-Allow-Origin"] = "*"; // Temporary - cors_domain not working - need future fix
 }
 
 void HttpResponse::setStatus(HTTP_STATUS s) {
