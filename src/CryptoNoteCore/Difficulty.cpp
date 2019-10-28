f07a855ba9948ebee5e3818925395d1c0c2a674c
2ACoin Miner <miner@2acoin.org>
2019-10-28 15:10:55 -0500

    Standardize on turtlecoin CORE vs bytecoin

--- src/CryptoNoteCore/Difficulty.cpp
+++ src/CryptoNoteCore/Difficulty.cpp
@@ -1,15 +1,14 @@
 // Copyright (c) 2018, The TurtleCoin Developers
-// Copyright (c) 2018, 2ACoin Developers
+// Copyright (c) 2018,2019 2ACoin Developers
 // 
 // Please see the included LICENSE file for more information.
 
-#include <algorithm>
-
 #include "Difficulty.h"
 
+#include <algorithm>
 #include <config/CryptoNoteConfig.h>
 
-// LWMA-2 difficulty algorithm 
+// LWMA-2 difficulty algorithm
 // Copyright (c) 2017-2018 Zawy, MIT License
 // https://github.com/zawy12/difficulty-algorithms/issues/3
 uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties)
@@ -24,34 +23,34 @@ uint64_t nextDifficultyV5(std::vector<uint64_t> timestamps, std::vector<uint64_t
     }
 
     for (int64_t i = 1; i <= N; i++)
-    {  
-        ST = static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i-1]);
+    {
+        ST = static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i - 1]);
 
         ST = std::max(-4 * T, std::min(ST, 6 * T));
 
-        L +=  ST * i; 
+        L += ST * i;
 
-        if (i > N-3)
+        if (i > N - 3)
         {
             sum_3_ST += ST;
-        } 
+        }
     }
 
     next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
 
-    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N-1];
+    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N - 1];
 
     next_D = std::max((prev_D * 67) / 100, std::min(next_D, (prev_D * 150) / 100));
 
     if (sum_3_ST < (8 * T) / 10)
-    {  
+    {
         next_D = std::max(next_D, (prev_D * 108) / 100);
     }
 
     return static_cast<uint64_t>(next_D);
 }
 
-// LWMA-2 difficulty algorithm 
+// LWMA-2 difficulty algorithm
 // Copyright (c) 2017-2018 Zawy, MIT License
 // https://github.com/zawy12/difficulty-algorithms/issues/3
 uint64_t nextDifficultyV4(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties)
@@ -66,32 +65,32 @@ uint64_t nextDifficultyV4(std::vector<uint64_t> timestamps, std::vector<uint64_t
     }
 
     for (int64_t i = 1; i <= N; i++)
-    {  
-        ST = clamp(-6 * T, static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i-1]), 6 * T);
+    {
+        ST = clamp(-6 * T, static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i - 1]), 6 * T);
 
-        L +=  ST * i; 
+        L += ST * i;
 
-        if (i > N-3)
+        if (i > N - 3)
         {
             sum_3_ST += ST;
-        } 
+        }
     }
 
-    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
-    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N-1];
+    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N + 1) * 99) / (100 * 2 * L);
+    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N - 1];
 
     /* Make sure we don't divide by zero if 50x attacker (thanks fireice) */
-    next_D = std::max((prev_D*67)/100, std::min(next_D, (prev_D*150)/100));
+    next_D = std::max((prev_D * 67) / 100, std::min(next_D, (prev_D * 150) / 100));
 
     if (sum_3_ST < (8 * T) / 10)
-    {  
+    {
         next_D = std::max(next_D, (prev_D * 110) / 100);
     }
 
     return static_cast<uint64_t>(next_D);
 }
 
-// LWMA-2 difficulty algorithm 
+// LWMA-2 difficulty algorithm
 // Copyright (c) 2017-2018 Zawy, MIT License
 // https://github.com/zawy12/difficulty-algorithms/issues/3
 uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties)
@@ -107,25 +106,25 @@ uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t
     }
 
     for (int64_t i = 1; i <= N; i++)
-    {  
-        ST = std::max(-FTL, std::min(static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i-1]), 6 * T));
+    {
+        ST = std::max(-FTL, std::min(static_cast<int64_t>(timestamps[i]) - static_cast<int64_t>(timestamps[i - 1]), 6 * T));
 
-        L +=  ST * i; 
+        L += ST * i;
 
-        if (i > N-3)
+        if (i > N - 3)
         {
             sum_3_ST += ST;
-        } 
+        }
     }
 
-    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N+1) * 99) / (100 * 2 * L);
-    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N-1];
+    next_D = (static_cast<int64_t>(cumulativeDifficulties[N] - cumulativeDifficulties[0]) * T * (N + 1) * 99) / (100 * 2 * L);
+    prev_D = cumulativeDifficulties[N] - cumulativeDifficulties[N - 1];
 
     /* Make sure we don't divide by zero if 50x attacker (thanks fireice) */
-    next_D = std::max((prev_D*70)/100, std::min(next_D, (prev_D*107)/100));
+    next_D = std::max((prev_D * 70) / 100, std::min(next_D, (prev_D * 107) / 100));
 
     if (sum_3_ST < (8 * T) / 10)
-    {  
+    {
         next_D = (prev_D * 110) / 100;
     }
 
