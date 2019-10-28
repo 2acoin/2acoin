441b4842f25e2ecb1dcb603016b44ef947956274
2ACoin Miner <miner@2acoin.org>
2019-10-28 12:09:15 -0500

    Update cmakelist for external files

--- external/cpp-httplib/httplib.h
+++ external/cpp-httplib/httplib.h
@@ -3,7 +3,6 @@
 //
 //  Copyright (c) 2017 Yuji Hirose. All rights reserved.
 //  Copyright (c) 2018-2019 The TurtleCoin Developers. All rights reserved.
-//  Copyright (c) 2019 2ACoin Developers. All rights reserved.
 //  MIT License
 //
 
